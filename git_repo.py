168c138cf75be7885aad0016e15f055aed6730ee
sarthoksarik <kirasnamdas@gmail.com>
2019-10-24 09:05:34 +0600

    incomplete

---
+++ git_repo.py
@@ -0,0 +1,30 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# make an api call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status Code: {r.status_code}")
+
+# process results
+response_dict = r.json()
+repo_dicts = response_dict['items']
+
+repo_names, stars = [], []
+
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# make visualizatiom
+data = [{
+        'type': 'Bar',
+        'x': repo_names,
+        'y': stars,
+        }]
+my_layout = {
+
+}
