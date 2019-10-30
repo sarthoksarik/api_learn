44338e68ce0e6a5555018a726538916bdbf76a44
sarthoksarik <kirasnamdas@gmail.com>
2019-10-30 18:44:55 +0600

    jj

--- git_repo.py
+++ git_repo.py
@@ -13,17 +13,22 @@ print(f"Status Code: {r.status_code}")
 response_dict = r.json()
 repo_dicts = response_dict['items']
 
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
 
 # make visualizatiom
 data = [{
         'type': 'bar',
         'x': repo_names,
         'y': stars,
+        'hovertext': labels,
         'marker': {
             'color': 'rgb(60, 40, 120)',
             'line': {
